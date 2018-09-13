6462e4ab43927a150d9eb1e9ebdf2ce0ee2fa4bd
musphilo <fm_bouguesri@esi.dz>
2018-09-13 12:20:58 +0100

    using the reference of functions

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ class NameGenerator {
         const btn = document.querySelector('button');
         //btn.addEventListener('click', addName()); 
         //This syntax will make JS look for a variable or function on the constructor then the document (and not in the current class) so we'll get an error
-        btn.addEventListener('click', this.addName()); 
+        //btn.addEventListener('click', this.addName()); 
         //This syntax will use the function in the class but it'll execute it immediately 
+        btn.addEventListener('click', this.addName);
+        //this syntax will add the reference of the function so that when a click event occures the listener will know to what address it goes, and it works fine but in some cases not
     }
     
     addName() {
